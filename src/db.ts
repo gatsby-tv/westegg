ffb5772106e455ba78db716a76cb40d5e6676d4a
Jacob Greenway <jagreenway12@gmail.com>
2021-01-03 16:35:57 -0600

    add auth source to db connection string

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,7 @@
-import { connect } from "http2";
-import mongoose, { Connection } from "mongoose";
+import mongoose from "mongoose";
 
 // Create connection string to mongodb
-const connectionString = `${process.env.MONGO_PROTOCOL}://gatsby:${process.env.MONGO_API_PASS}@${process.env.MONGO_HOST}/gatsby?retryWrites=true&w=majority`;
+const connectionString = `${process.env.MONGO_PROTOCOL}://gatsby:${process.env.MONGO_API_PASS}@${process.env.MONGO_HOST}/gatsby?retryWrites=true&w=majority&authSource=admin`;
 
 const db = {
   connect: async () => {
