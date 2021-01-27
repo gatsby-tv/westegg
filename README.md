d2cba5f6342e4ede3193e4d7e7c8c84769482fc3
Jacob Greenway <jagreenway12@gmail.com>
2021-01-27 00:27:04 -0600

    add ipfs container to docker-compose to pin avatars (very early, no auth)

--- README.md
+++ README.md
@@ -1,39 +1,24 @@
 # WestEgg
 
-West Egg is the backend for the Gatsby hub site and contains indexing data on users, channels, videos and more.
-
-## Run development build
-1. Install NodeJS 12 and Docker
-2. Install dependencies `npm install`
-3. Run mongo docker image locally
-```
-docker run -d \
-  --name gatsby_mongo \
-  -e MONGO_INITDB_ROOT_USERNAME=root \
-  -e MONGO_INITDB_ROOT_PASSWORD=root \
-  -e MONGO_API_USERNAME=gatsby \
-  -e MONGO_API_PASSWORD=gatsby \
-  -e MONGO_INITDB_DATABASE=gatsby \
-  -v db:/data/db \
-  -v mongo:/docker-entrypoint-initdb.d \
-  -p 27017:27017 \
-  mongo:4.4.1
-```
-4. Copy `default.env` as `.env` and fill out the environment variables, eg.
-```
-# DB Configuration
-MONGO_PROTOCOL=mongodb+srv
-MONGO_HOST=localhost
-MONGO_PORT=27017
-MONGO_ROOT_PASS=root
-MONGO_API_PASS=gatsby
-
-JWT_SECRET=localhost
-```
-5. Run the server `npm start`
+WestEgg is the backend for the Gatsby hub site and contains indexing data on users, channels, videos and more.
 
 ## Run development build (docker-compose)
 1. Install docker and docker-compose
 2. Run `docker-compose up --build`
 
-## TODO: Build and deploy production
+## Run development build (just westegg api server)
+1. Install NodeJS 12 and Docker
+2. Install dependencies `npm install`
+3. Copy `default.env` as `.env` and fill out the environment variables, eg.
+```
+# DB Configuration
+MONGO_PROTOCOL=mongodb
+MONGO_HOST=localhost
+MONGO_ROOT_PASS=root
+MONGO_API_PASS=gatsby
+# IPFS
+IPFS_URL=http://localhost:5001
+
+JWT_SECRET=localhost
+```
+4. Run the server `npm start`
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,13 +2,18 @@ import {
   ErrorMessage,
   GetUserAccountRequest,
   NotFound,
+  PutUserRequest,
   StatusCode
 } from "@gatsby-tv/types";
 import { Request, Router } from "express";
 import * as ExpressCore from "express-serve-static-core";
+import IPFSClient from "ipfs-http-client";
 import { User } from "../entities/User";
 
 const router = Router();
+const ipfs = IPFSClient({
+  url: process.env.IPFS_URL || "http://localhost:5001"
+});
 
 /**
  * GET /user/:handle
@@ -36,5 +41,47 @@ router.get(
     }
   }
 );
+
+/**
+ * PUT /user/:handle
+ */
+// TODO: Add auth middleware
+router.put("/:handle", async (req, res, next) => {
+  try {
+    const request: PutUserRequest = req.body;
+    const handle = req.params.handle;
+
+    // Get the user to update
+    const user = await User.findById(handle);
+
+    if (!user) {
+      throw new NotFound(ErrorMessage.USER_NOT_FOUND);
+    }
+
+    // Update avatar
+    if (request.avatar) {
+      // TODO: Validate mime type is allowed
+      // TODO: use ipfs block stat or ipfs files stat
+      // TODO: Validate file contents
+      // TODO: Validate file size
+      // Pin on IPFS node/cluster
+      await ipfs.pin.add(request.avatar.hash);
+      // TODO: Unpin old avatar if pinned successfully
+      // Add new avatar metadata to user
+      user.avatar = request.avatar;
+    }
+
+    // TODO: Handle
+    // TODO: Name
+    // TODO: Description
+
+    // Save user
+    await user.save();
+    // TODO: as PutUserResponse
+    res.status(StatusCode.OK).json(user.toJSON());
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
