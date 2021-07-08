63f86cf3fdb1ad441bed9cdb1526ed218e70b94b
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-07-07 19:14:20 -0500

    Added SendGrid API Key validation

--- src/environment.ts
+++ src/environment.ts
@@ -9,6 +9,7 @@ export enum Environment {
 const SECRET_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\!\@\#\$\%\^\&\*\(\)\_\+])[0-9a-zA-Z\!\@\#\$\%\^\&\*\(\)\_\+\/\,\.\<\\\>\{\}\[\]\;\:\`\-\+\=]{64,}$/;
 const IPFS_HOST_REGEX = /^http(s)?:\/\/[a-zA-Z0-9\%\.\_\+\~\#\=]{1,256}\:[0-9]{1,5}([a-zA-Z0-9\(\)\@\:\%\_\+\.\~\#\?\&\/\=]*)$/;
 const MONGO_HOST_REGEX = /^[a-zA-Z0-9\%\.\_\+\~\#\=\-]{1,256}$/;
+const SENDGRID_API_KEY_REGEX = /^SG\..{1,256}$/;
 
 // Check all environment variables are properly set and valid, exit if not
 export function validateEnvironment() {
@@ -36,7 +37,8 @@ export function validateEnvironment() {
     }
 
     // Verify environment variables
-    if (Environment.PRODUCTION === process.env.WESTEGG_ENV) {
+    if (Environment.PRODUCTION === process.env.WESTEGG_ENV ||
+      Environment.STAGING === process.env.WESTEGG_ENV) {
       let regexMatch = process.env.JWT_SECRET?.match(SECRET_REGEX) as string[];
       let matchString = regexMatch?.join("");
       if (matchString !== process.env.JWT_SECRET) {
@@ -44,6 +46,7 @@ export function validateEnvironment() {
       }
     }
 
+    // Validate MongoDB related environment variables
     if (
       !process.env.MONGO_PROTOCOL ||
       (process.env.MONGO_PROTOCOL !== "mongodb" &&
@@ -52,25 +55,48 @@ export function validateEnvironment() {
       throw new Error("MongoDB protocol missing or malformed!");
     }
 
-    let regexMatch = process.env.MONGO_HOST?.match(
-      MONGO_HOST_REGEX
-    ) as string[];
-    let matchString = regexMatch?.join("");
-    if (!process.env.MONGO_HOST || matchString !== process.env.MONGO_HOST) {
-      throw new Error("MongoDB host missing or malformed!");
+    if (!process.env.MONGO_HOST) {
+      throw new Error("MongoDB host missing!");
+    }
+    else {
+      let regexMatch = process.env.MONGO_HOST?.match(
+        MONGO_HOST_REGEX
+      ) as string[];
+      let matchString = regexMatch?.join("");
+      if (!process.env.MONGO_HOST || matchString !== process.env.MONGO_HOST) {
+        throw new Error("MongoDB host missing or malformed!");
+      }
     }
 
     if (!process.env.MONGO_API_PASS) {
       throw new Error("MongoDB api password missing!");
     }
 
-    if (Environment.DEV === process.env.WESTEGG_ENV) {
+    if (Environment.PRODUCTION === process.env.WESTEGG_ENV ||
+      Environment.STAGING === process.env.WESTEGG_ENV) {
       let regexMatch = process.env.IPFS_URL?.match(IPFS_HOST_REGEX) as string[];
       let matchString = regexMatch?.join("");
       if (matchString !== process.env.IPFS_URL || !process.env.IPFS_URL) {
         throw new Error("IPFS host missing or malformed!");
       }
     }
+
+    // Validate Sendgrid API key
+    if (!process.env.SENDGRID_API_KEY) {
+      throw new Error("SendGrid API key missing!");
+    }
+
+    if (Environment.PRODUCTION === process.env.WESTEGG_ENV ||
+      Environment.STAGING === process.env.WESTEGG_ENV) {
+      let regexMatch = process.env.SENDGRID_API_KEY?.match(
+        SENDGRID_API_KEY_REGEX
+      ) as string[];
+      let matchString = regexMatch?.join("");
+      if (matchString !== process.env.SENDGRID_API_KEY) {
+        throw new Error("SendGrid API key malformed!");
+      }
+    }
+
   } catch (error) {
     logger.error(`FATAL: ${error}`);
     process.exit(1);
