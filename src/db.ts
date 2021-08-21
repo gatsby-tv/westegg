7477f330a45ea4eef2635c92c6cac2e1a09af7f1
Jacob Greenway <jagreenway12@gmail.com>
2021-08-21 15:51:33 -0500

    bugfix: Fix deprecated mongoose connection setup (#82)

--- src/db.ts
+++ src/db.ts
@@ -2,14 +2,16 @@ import mongoose from "mongoose";
 
 const params = new URLSearchParams();
 params.set("retryWrite", "true");
-params.set("w", "majority");
+params.set("writeConcern", "majority");
 params.set("authSource", "admin");
 
 const db = {
   connect: async () => {
     await mongoose.connect(`${process.env.MONGO_URL}?${params}`, {
       useFindAndModify: false,
-      useCreateIndex: true
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
   }
 };
